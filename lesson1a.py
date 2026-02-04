afdc1bcea2a41b9aa0bab62cbee4bbb62d2bcc9b
unknown <stormyz22a@gmail.com>
2026-02-04 13:07:16 +0300

    updated codes

---
+++ lesson1a.py
@@ -0,0 +1,34 @@
+# What is a variable: Variable are labels which acts as containers to hod values. In python, a variable is always associated with some values.
+#We use the assignment operator (=) to assign values to a variable.
+# Anything that is inside quotes is called a string
+message = 'Hello there! Hope you are doing well?'
+print(message)
+
+message2 = " Goodbye"
+print(message2)
+
+
+# Concactinating variable- joining two or more outputs together
+message3 = message + message2
+print(message3)
+
+answer = 1+2
+print(answer)
+
+answer = 16-3
+print(answer)
+
+# Variables can be named using a number, letter and an underscore. They can start with a letter and an underscore but not witha number
+
+name_1 = "Cedric"
+print(name_1)
+
+# Variable names cannot contain spaces. We can use underscore to separate the names
+
+total_marks = 100
+print(total_marks)
+
+# Variable names are not supposed to be keywords/ reserved characters in python
+#for = Nairobi
+#print(for)
+
---
+++ tesy.py
@@ -0,0 +1,12 @@
+print("Hello there Cedric")
+
+# This is a comment
+"""This is another comment"""
+'''This is another docstring'''
+
+message = 'Hello, world!'
+print(message)
+
+message = 'Good Bye!'
+print(message)
+
