5559c87243c554419687bb8885aa0cca051f9d21
unknown <stormyz22a@gmail.com>
2026-02-04 13:22:35 +0300

    added code

---
+++ lesson1b.py
@@ -0,0 +1,18 @@
+# Python data types
+# String - It is a series of characters that are inside of quotes.
+name = "Cedric Kipngeno"
+print(name)
+print(type(name))
+
+# Numeric
+# Int - This indicates a whole number in python. For an interger, we don't use quotes
+age = 39
+print("The age of a person is: ", age)
+print(type(age))
+
+# Float - this is a number with a decimal
+coordinates = 27.6946
+print("The coordinates are :", coordinates)
+print(type(coordinates))
+
+# Research and come up with an example of complex data type
