e266f3b30be54ba28401df795c24e5b71395db5a
unknown <stormyz22a@gmail.com>
2026-02-04 13:58:50 +0300

    added codes

---
+++ lesson1c.py
@@ -0,0 +1,44 @@
+# Arithmetic operations
+# Addition
+number1 =  10
+number2 =  20
+addition = number1 + number2
+print(addition)
+print("The sum of the two numbers is:", addition)
+
+# Subtraction
+number3 = 65
+number4 = 25
+difference = number3 - number4
+print(difference)
+
+# Multiplication
+number5 = 5
+number6 = 6
+product = number5 * number6
+print(product)
+
+#Division
+number7 = 64
+number8 = 8
+quotient = number7 / number8
+print(quotient)
+
+#Modulus - This means the remainder of a number when it gets sub-divided by another one.
+number9 = 14
+number10 = 5
+modulus = number9 % number10
+print(modulus)
+
+# Floor division - This is the number of times a number subdivides ignoring the remainder
+number9 = 14
+number10 = 5
+floor = number9 // number10
+print(floor)
+
+
+# Exponent (**)  
+# This is the power of a huge number
+number11 = 10
+exponent = number11 ** 4
+print(exponent)
