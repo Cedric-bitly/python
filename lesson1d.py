b3cb9e2522b1a413b82431c43edb16d32ef47bd8
unknown <stormyz22a@gmail.com>
2026-02-04 22:39:42 +0300

    updated codes

---
+++ lesson1d.py
@@ -0,0 +1,34 @@
+# Create a python program that is able to calculate the simple interst given the principle as 5000, rate as 5% and time as 8 years. SI= (p * R * t)/1000
+principle = 50000
+rate = 5
+time = 8
+simple_interest = (principle * rate * time)/100
+print(simple_interest)
+
+  
+
+# Assignment
+# 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI = (weight) / (height squared)
+# 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+# Research on python list, tuple and Dictionary Data types.
+
+# Assignment 1
+# BMI = (weight / (height**2))
+weight  = 78
+height = 1.75
+BMI = weight / (height ** 2)
+print("BMI is:", BMI )
+
+# Assignment 2
+
+# Area = L * W
+width  = 25
+length = 48
+Area  = width * length
+print("Area is:", Area )
+
+# Perimeter = ((L * 2) + (W * 2))
+width = 25
+length = 48
+Perimeter  = ((length * 2) + (width * 2))
+print("Perimeter is:", Perimeter)
