0beb4d86cfadaa78e3be47afdb3429838dc3aaf5
unknown <stormyz22a@gmail.com>
2026-02-10 03:00:12 +0300

    updated codes

--- lesson4c.py
+++ lesson4c.py
@@ -1,13 +1,19 @@
 # For loop
 # A for loop can also be used to iterate through a list, tuple, string or even a dictionary.
-
-#
+ 
 player  = {
     "name" : "Mbappe",
     "age" : 25,
-    "teams" : "PSG" "Monaco" "France",
+    "teams" : ["PSG", "Monaco", "France"],
     "nationality" : "French"
 }
 
+for key in player:
+    print(key)
+print("=============")
+for values in player:
+    print(player[values])
+
+print("=============")
 for team in player["teams"]:
     print(team)
