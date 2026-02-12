fb945e1f6843604b2345a50867c5612b22b594a7
unknown <stormyz22a@gmail.com>
2026-02-12 11:33:55 +0300

    updated codes

--- lesson6e.py
+++ lesson6e.py
@@ -6,3 +6,32 @@ try:
 except Exception as e:
     print("There is an error:", e)
 
+
+try:
+    age = int(input("Enter your age: "))
+    print("Next year you will be:", age + 1)
+except Exception as e:
+    print("There is an error:", e)
+
+
+try:
+    numbers = [10, 20, 30]
+    print("The number is:", numbers[2])
+except Exception as e:
+    print("There is an error:", e)
+
+
+try:
+    marks = int(input("Enter your marks: "))
+    print("Your marks are:", marks)
+except ValueError as e:
+    print("Invalid input! Please enter a number.")
+
+
+
+import math
+try:
+     result = math.sqrt(9)
+     print("Result:", result)
+except ValueError as e:
+     print("There is a ValueError:", e)
